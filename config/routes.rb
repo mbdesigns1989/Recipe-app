0ac401318d88e25c4718b857a5a82d551296a9bb
mbdesigns1989 <mbdesigns1989@yahoo.com>
2022-08-31 03:22:28 +0300

    Fix n+1

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'shopping_list/show'
+  get 'recipe_foods/new'
+  resources :foods
+  devise_for :users
+  resources :recipes, only: [:index, :show, :new, :create, :destroy, :put ] do
+    resources :recipe_foods, path: 'food', only: [:destroy, :new, :create]
+  end
+
+  match 'recipes/:recipe_id' => 'recipes#toogle_public', as: :toogle_public, via: :patch
+  match 'public_recipes' => 'recipes#public_recipes', as: :public_recipes, via: :get
+
+  root 'recipes#public_recipes'
+end
