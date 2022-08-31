7418a569ad24292d03105a0fdb4cfe279a305fee
mandela.t <manderat206@gmail.com>
2022-08-31 19:33:33 +0200

    Modifie the files

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,20 +1,14 @@
 class ShoppingListController < ApplicationController
-  def show
+   def show
     @user = current_user
-    @recipes = @user.recipes
-    @recipe_food = []
-    @recipes.includes([:recipe_foods]).each do |recipe|
-      recipe.recipe_foods.each do |recipe_food|
-        @recipe_food.push(recipe_food)
-      end
-    end
-    @total_items = 0
+    @foods = @user.foods
+    @recipes = Recipe.where(user_id: current_user)
     @total_price = 0
-    @recipes.each do |items|
-      items.recipe_foods.includes(:food).each do |item|
-        @total_items += item.quantity
-        @total_price += item.quantity * item.food.price
-      end
+
+    @quantity = 0
+
+    @foods.each do |food|
+      @quantity += RecipeFood.joins(:food).where(food_id: food.id).sum('quantity') * food.price
     end
   end
 end
